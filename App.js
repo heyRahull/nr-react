fca1f3c817cf08242c70b10ab3996b1d9c75bc04
Rahul Agarwal <rahul123agarwal99@gmail.com>
2024-02-29 15:32:32 +0530

    session 3- Igniting our app

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-const heading1 = React.createElement("h1", {}, "Heading1");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading1 = React.createElement("h1", {}, "Heading1 for parcel");
 const heading2 = React.createElement("h2", {}, "Heading2");
 const container = React.createElement("div", { id: "container" }, [
   heading1,
