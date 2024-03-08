52cd9c5ec3eec548b463f00d56be57ab259216f8
Rahul Agarwal <rahul123agarwal99@gmail.com>
2024-03-08 10:16:28 +0530

    session10 code

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Contact from "./components/Contact";
 import Error from "./components/Error";
 import { Outlet } from "react-router-dom";
 import RestMenu from "./components/RestMenu";
+import Profile from "./components/Profile";
 
 /*
   Header
@@ -46,12 +47,18 @@ const appRouter = createBrowserRouter([
     errorElement: <Error />,
     children: [
       {
-        path: "/",
+        path: "",
         element: <Body />,
       },
       {
         path: "/about",
         element: <About />,
+        children: [
+          {
+            path: "profile",
+            element: <Profile />,
+          },
+        ],
       },
       {
         path: "/contact",
--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,11 @@
+import { Outlet } from "react-router-dom";
 const About = () => {
-  return <h1>About Us</h1>;
+  return (
+    <>
+      <h1>About Us</h1>
+      <Outlet />
+    </>
+  );
 };
 
 export default About;
